cbbddc65eca19886e5f07ff1623afa73e8ff81ab
Peter Kfoury <pkfoury@purdue.edu>
2018-11-08 17:17:28 -0500

    pravin

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 require('dotenv').config()
 const login = require('facebook-chat-api')
+const fs = require('fs')
 
 
 login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD }, (err, api) => {
   if(err) return console.error(err);
 
+  // TODO: require('body-parser')
+
   api.listen((err, message) => {
     let msg = message.body
-    if(msg.substring(0,1) == '/'){
-      api.sendMessage(msg, message.threadID)
+    console.log(msg);
+    console.log(msg.indexOf)
+
+    // if(msg.startswith('/echo')){
+    if(0) {
+      api.sendMessage(msg.substring(7, msg.length), message.threadID)
+    }
+
+    else if(msg.indexOf('!pravin') == 0) {
+      let reply = {
+        attachment: fs.createReadStream(__dirname + '/pravin.jpg')
+      }
+      api.sendMessage(reply, message.threadID);
     }
   })
 })
