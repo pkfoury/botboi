38fbe622c92c7d72ea07646c6edea84d7d648bab
Peter Kfoury <pkfoury@purdue.edu>
2019-03-08 18:47:17 -0500

    😛

--- app.js
+++ app.js
@@ -9,34 +9,35 @@ const rl = readline.createInterface({
 });
 
 
-login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD }, (err, api) => {
-  if(err) {
-  	switch (err.error) {
-		case 'login-approval':
-			console.log('enter 2fa code:');
-			rl.on('line', (line) => {
-				err.continue(line);
-				rl.close();
-			});
-			break;
-		default:
-			console.log(err);
-		}
-	return;
+login({ email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD }, (err, api) => {
+  if (err) {
+    switch (err.error) {
+      case 'login-approval':
+        console.log('enter 2fa code:');
+        rl.on('line', (line) => {
+          err.continue(line);
+          rl.close();
+        });
+        break;
+      default:
+        console.log(err);
+    }
+    return;
   }
-  
 
   api.listen((err, message) => {
     const msg = message.body
-
-    if(msg.indexOf('/echo') == 0){
-      api.sendMessage(msg.substring(6, msg.length), message.threadID)
-    }
-
-    else if(msg.indexOf('/pravin') == 0) {
+    // if (msg.indexOf('/echo') == 0) {
+    //   api.sendMessage(msg.substring(6, msg.length), message.threadID)
+    // }
+    if (msg.indexOf('/pravin') == 0) {
       let reply = {
-        body: msg.substring(7, msg.length),
         attachment: fs.createReadStream(__dirname + '/pravin.jpg')
+      }
+      api.sendMessage(reply, message.threadID);
+    } else if (msg.indexOf('😛') == 0) {
+      let reply = {
+        attachment: fs.createReadStream(__dirname + '/pravin2.jpg')
       }
       api.sendMessage(reply, message.threadID);
     }
